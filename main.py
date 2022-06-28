5d45574c4039ff2fc04270f58b69428e1ae9b7f0
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-06-28 22:42:35 +0300

    feat: We created a dataset of words containing Turkish slang.

---
+++ main.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import codecs
+
+URL = "https://www.turkedebiyati.org/argo-sozlugu/"
+FIRSTWORD = "abondone"
+BANNEDWORD = "argo"
+
+source = requests.get(URL).text
+soup = BeautifulSoup(source, 'lxml')
+data = soup.find_all("p strong")
+
+p_strong_elements = []
+state = False
+
+csv_file = open('TurkceArgoKelimeler.csv', 'w', encoding="utf-16", newline="")
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow([''])
+
+
+def checkWord(word):
+    global state
+    if FIRSTWORD in word:
+        state = True
+    if state is True and BANNEDWORD not in word.lower():
+        p_strong_elements.append(word)
+
+
+for word in soup.find_all("strong"):
+    checkWord(str(word.text))
+
+for element in p_strong_elements:
+    for i in element:
+        test_string = element.replace(i, '')
+    csv_writer.writerow([test_string])
+csv_file.close()
