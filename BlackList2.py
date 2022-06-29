73beefd5b3045ec3fe9a59580593a2004208ec98
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-06-29 23:10:08 +0300

    feat: we added new elements to blacklist

---
+++ BlackList2.py
@@ -0,0 +1,28 @@
+from cgi import test
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+URL = "https://github.com/ooguz/turkce-kufur-karaliste/blob/master/karaliste.txt"
+source = requests.get(URL).text
+soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('TurkceArgoKelimeler.csv', 'a', encoding="utf-16", newline="")
+csv_writer = csv.writer(csv_file)
+
+table = soup.find('table')
+rows = table.findAll('tr')
+x = 0
+
+for tr in rows:
+    cols = tr.findAll(
+        'td', attrs={'class': 'blob-code blob-code-inner js-file-line'})
+    if not cols:
+        # Boş satırı geçer
+        continue
+    for td in cols:
+        word = str(td.text)
+        print(word.replace(',', ''))
+        csv_writer.writerow([word.replace(',', '')])
+    x = x + 1
+csv_file.close()
